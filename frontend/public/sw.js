7e7f10a9ae8cd2e6759f6786e16cdafe57d8c07a
festimii <festimi-rrezoni@hotmail.com>
2025-11-03 08:11:22 +0100

    tett

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,9 +1,11 @@
-// public/sw.js
 self.addEventListener("push", (event) => {
   const data = event.data?.json() || {};
   const title = data.title || "New Notification";
+
   const options = {
     body: data.body || "",
+    data: data.data || {},
+    actions: data.actions || [],
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
@@ -11,5 +13,12 @@ self.addEventListener("push", (event) => {
 
 self.addEventListener("notificationclick", (event) => {
   event.notification.close();
-  event.waitUntil(clients.openWindow("/"));
+
+  if (event.action === "open" && event.notification.data?.requestId) {
+    event.waitUntil(
+      clients.openWindow(`/requests/${event.notification.data.requestId}`)
+    );
+  } else {
+    event.waitUntil(clients.openWindow("/"));
+  }
 });
