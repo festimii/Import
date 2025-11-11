5f990e1a437991896ea335214ffbb1585e082519
festimii <festimi-rrezoni@hotmail.com>
2025-11-11 14:39:58 +0200

    te

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ app.get("/", (req, res) => res.send("✅ Import Tracker API Running"));
 startWmsOrdersSync();
 
 const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+});
